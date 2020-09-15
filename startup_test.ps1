ba52fa7356b72e85e72d5bf5892d1ed9e5fd47ea
rljohnson32 <rljohnson32@wisc.edu>
2020-09-15 15:57:27 -0500

    Add files via upload

---
+++ startup_test.ps1
@@ -0,0 +1,9 @@
+#PreReqs:
+# 1. Windows 2008 or Higher (HC Windows Server 2016 image included)
+# 2. Powershell 3.0 or Higher (HC Windows Server 2016 image included)
+# 3. .NET 4.62 or Higher (HC Windows Server 2016 image included)
+# 4. VS Build Tools (Needs to be installed)
+#create dir for installers
+mkdir C:\devops
+#move to directory
+cd C:\devops
